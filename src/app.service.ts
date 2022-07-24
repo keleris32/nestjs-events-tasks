56b1d893b6751846185c010c75be9639f3f5c8d1
keleris32 <charles.ejiegbu@gmail.com>
2022-07-24 16:43:45 +0100

    feat: added event emitter to decouple func

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { EventEmitter2, OnEvent } from '@nestjs/event-emitter';
+import { CreateUserDto } from './dto/create-user.dto';
+import { CreatedUserEvent } from './events/created-user.event';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly eventEmitter: EventEmitter2) {}
+
+  private readonly logger = new Logger(AppService.name);
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async createUser(body: CreateUserDto) {
+    this.logger.log('Creating user >> ', body);
+
+    const userId = '16532';
+    this.eventEmitter.emit(
+      'created.user',
+      new CreatedUserEvent(userId, body.email),
+    );
+
+    return 'User Created Successfully';
+  }
+
+  // React to event on a separate thread to avoid blocking response to client
+  @OnEvent('created.user')
+  welcomeNewUser(payload: CreatedUserEvent) {
+    this.logger.log('Welcoming new user >>', payload.email);
+  }
+
+  @OnEvent('created.user', { async: true })
+  async sendWelcomeGift(payload: CreatedUserEvent) {
+    this.logger.log('Sending welcome gift >>', payload.email);
+
+    await new Promise<void>((resolve) => setTimeout(() => resolve(), 3000));
+    this.logger.log('Welcome gift sent!');
   }
 }
