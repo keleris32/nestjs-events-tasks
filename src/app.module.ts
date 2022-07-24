5d9fbb97e8af579d132e7834dc9cf24dee6ea1c4
keleris32 <charles.ejiegbu@gmail.com>
2022-07-24 15:11:36 +0100

    feat: added EventEmitter and Schedule module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [EventEmitterModule.forRoot(), ScheduleModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
